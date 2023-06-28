505af00a5d7201fdd6acd8f6c4f1a2f8775a7c10
Hyun-Tae Jin <htjmario@gmail.com>
2023-06-27 22:33:10 -0400

    fixed tabbed formatting

--- js/getWeather.js
+++ js/getWeather.js
@@ -1,69 +1,69 @@
 async function fetchWeatherData(city, zip) {
-    const apiKey = "056e5a496ed237e46fe2e65ead648222";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&`;
+  const apiKey = "056e5a496ed237e46fe2e65ead648222";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&`;
 
-    if (zip) {
-        apiUrl += `zip=${zip},us`;
-    } else {
-        apiUrl += `q=${city}`;
-    }
+  if (zip) {
+    apiUrl += `zip=${zip},us`;
+  } else {
+    apiUrl += `q=${city}`;
+  }
 
-    try {
-        const response = await axios.get(apiUrl);
-        return response.data;
-    } catch (error) {
-        alert(
-            "Error fetching weather data. Please check your input and try again."
-        );
-        console.error(error);
-    }
+  try {
+    const response = await axios.get(apiUrl);
+    return response.data;
+  } catch (error) {
+    alert(
+      "Error fetching weather data. Please check your input and try again."
+    );
+    console.error(error);
+  }
 }
 
 function updateBackgroundImage(data) {
-    const weatherElement = document.querySelector(".bg-cover");
-    const weatherDisplay = document.getElementById("weather");
-    const cityName = data.name;
-    const weather = data.weather[0].main;
-    const windSpeed = data.wind.speed * 2.237; // Convert wind speed to mph
+  const weatherElement = document.querySelector(".bg-cover");
+  const weatherDisplay = document.getElementById("weather");
+  const cityName = data.name;
+  const weather = data.weather[0].main;
+  const windSpeed = data.wind.speed * 2.237; // Convert wind speed to mph
 
-    let backgroundImage, iconImage, weatherDesc;
+  let backgroundImage, iconImage, weatherDesc;
 
-    if (windSpeed > 20) {
-        backgroundImage = "static/images/windy.jpg";
-        iconImage = "static/images/wind-icon.svg";
-        weatherDesc = "Windy";
-    } else {
-        switch (weather.toLowerCase()) {
-            case "clouds":
-                backgroundImage = "static/images/cloudy.jpg";
-                iconImage = "static/images/cloud-icon.svg";
-                weatherDesc = "Cloudy";
-                break;
-            case "rain":
-                backgroundImage = "static/images/rainy.jpg";
-                iconImage = "static/images/rain-icon.svg";
-                weatherDesc = "Raining";
-                break;
-            case "snow":
-                backgroundImage = "static/images/snowy.jpg";
-                iconImage = "static/images/snow-icon.svg";
-                weatherDesc = "Snowing";
-                break;
-            case "thunderstorm":
-                backgroundImage = "static/images/stormy.jpg";
-                iconImage = "static/images/storm-icon.svg";
-                weatherDesc = "Thunderstorming";
-                break;
-            default:
-                backgroundImage = "static/images/sunny.jpg";
-                iconImage = "static/images/sun-icon.svg";
-                weatherDesc = "Clear";
-                break;
-        }
+  if (windSpeed > 20) {
+    backgroundImage = "static/images/windy.jpg";
+    iconImage = "static/images/wind-icon.svg";
+    weatherDesc = "Windy";
+  } else {
+    switch (weather.toLowerCase()) {
+      case "clouds":
+        backgroundImage = "static/images/cloudy.jpg";
+        iconImage = "static/images/cloud-icon.svg";
+        weatherDesc = "Cloudy";
+        break;
+      case "rain":
+        backgroundImage = "static/images/rainy.jpg";
+        iconImage = "static/images/rain-icon.svg";
+        weatherDesc = "Raining";
+        break;
+      case "snow":
+        backgroundImage = "static/images/snowy.jpg";
+        iconImage = "static/images/snow-icon.svg";
+        weatherDesc = "Snowing";
+        break;
+      case "thunderstorm":
+        backgroundImage = "static/images/stormy.jpg";
+        iconImage = "static/images/storm-icon.svg";
+        weatherDesc = "Thunderstorming";
+        break;
+      default:
+        backgroundImage = "static/images/sunny.jpg";
+        iconImage = "static/images/sun-icon.svg";
+        weatherDesc = "Clear";
+        break;
     }
+  }
 
-    weatherElement.style.backgroundImage = `url(${backgroundImage})`;
-    weatherDisplay.innerHTML = `
+  weatherElement.style.backgroundImage = `url(${backgroundImage})`;
+  weatherDisplay.innerHTML = `
         <div class="flex justify-center items-end space-x-4 mb-5">
             <div class="flex flex-col items-end">
                 <h1 class="text-base md:text-lg whitespace-nowrap">Right now in</h1>
@@ -75,17 +75,17 @@ function updateBackgroundImage(data) {
             </div>
         </div>
     `;
-    weatherDisplay.classList.add("relative", "bottom-24");
+  weatherDisplay.classList.add("relative", "bottom-24");
 }
 
 function getTemperature(data) {
-    const temperatureElement = document.getElementById("temperature");
-    const temp = convertKelvinToFahrenheit(data.main.temp);
-    const tempMin = convertKelvinToFahrenheit(data.main.temp_min);
-    const tempMax = convertKelvinToFahrenheit(data.main.temp_max);
-    const feelsLike = convertKelvinToFahrenheit(data.main.feels_like);
+  const temperatureElement = document.getElementById("temperature");
+  const temp = convertKelvinToFahrenheit(data.main.temp);
+  const tempMin = convertKelvinToFahrenheit(data.main.temp_min);
+  const tempMax = convertKelvinToFahrenheit(data.main.temp_max);
+  const feelsLike = convertKelvinToFahrenheit(data.main.feels_like);
 
-    temperatureElement.innerHTML = `
+  temperatureElement.innerHTML = `
         <div class="flex flex-col items-end">
             <div>Temperature: <span class="text-cyan-400">${temp}°F</span></div>
             <div>(Feels like): <span class="text-green-500" id="temp">${feelsLike}°F</span></div>
@@ -97,26 +97,26 @@ function getTemperature(data) {
         <div class="flex flex-col">
             <div>Min: <span class="text-blue-500">${tempMin}°F</span></div>
             <div>Max: <span class="text-red-500">${tempMax}°F</span></div>
-        </div>  
+        </div>
     `;
-    temperatureElement.classList.add("relative", "bottom-24");
+  temperatureElement.classList.add("relative", "bottom-24");
 }
 
 function convertKelvinToFahrenheit(kelvin) {
-    return Math.round((kelvin - 273.15) * 1.8 + 32);
+  return Math.round((kelvin - 273.15) * 1.8 + 32);
 }
 
 async function fetchAndUpdateWeather(city, zip) {
-    if (!city && !zip) {
-        alert("Please enter a city or zip code.");
-        return false;
-    }
-
-    const data = await fetchWeatherData(city, zip);
-    if (data) {
-        updateBackgroundImage(data);
-        getTemperature(data);
-        return true;
-    }
+  if (!city && !zip) {
+    alert("Please enter a city or zip code.");
     return false;
+  }
+
+  const data = await fetchWeatherData(city, zip);
+  if (data) {
+    updateBackgroundImage(data);
+    getTemperature(data);
+    return true;
+  }
+  return false;
 }
--- js/windowControl.js
+++ js/windowControl.js
@@ -1,145 +1,149 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const leftCurtain = document.getElementById("left-curtain");
-    const rightCurtain = document.getElementById("right-curtain");
-    const glassElement = document.getElementById("glass");
-    const windowElement = document.getElementById("window");
-    const curtainWrapper = document.getElementById("curtain-wrapper");
-    const backgroundWrapper = document.getElementById("background-wrapper");
-    const cityField = document.getElementById("city-field");
-    const zipField = document.getElementById("zip-field");
-    const temperatureElement = document.getElementById("temperature");
-    const weatherElement = document.getElementById("weather");
+  const leftCurtain = document.getElementById("left-curtain");
+  const rightCurtain = document.getElementById("right-curtain");
+  const glassElement = document.getElementById("glass");
+  const windowElement = document.getElementById("window");
+  const curtainWrapper = document.getElementById("curtain-wrapper");
+  const backgroundWrapper = document.getElementById("background-wrapper");
+  const cityField = document.getElementById("city-field");
+  const zipField = document.getElementById("zip-field");
+  const temperatureElement = document.getElementById("temperature");
+  const weatherElement = document.getElementById("weather");
 
-    cityField.addEventListener("input", filterCityInput);
-    zipField.addEventListener("input", filterZipInput);
-    cityField.addEventListener("input", enableCurtainInteraction);
-    cityField.addEventListener("keydown", handleEnterKey);
-    zipField.addEventListener("keydown", handleEnterKey);
-    zipField.addEventListener("input", toggleCityField);
-    curtainWrapper.addEventListener("click", handleCurtainClick);
-    backgroundWrapper.addEventListener("click", (e) => {
-        if (windowElement.classList.contains("-translate-y-full")) {
-            e.stopPropagation();
-            resetForm();
-            closeCurtains();
-            enableInputs();
-        }
-    });
+  cityField.addEventListener("input", filterCityInput);
+  zipField.addEventListener("input", filterZipInput);
+  cityField.addEventListener("input", enableCurtainInteraction);
+  cityField.addEventListener("keydown", handleEnterKey);
+  zipField.addEventListener("keydown", handleEnterKey);
+  zipField.addEventListener("input", toggleCityField);
+  curtainWrapper.addEventListener("click", handleCurtainClick);
+  backgroundWrapper.addEventListener("click", (e) => {
+    if (windowElement.classList.contains("-translate-y-full")) {
+      e.stopPropagation();
+      resetForm();
+      closeCurtains();
+      enableInputs();
+    }
+  });
 
-    function toggleCityField() {
-        if (zipField.value) {
-            cityField.disabled = true;
-        } else {
-            cityField.disabled = false;
-        }
+  function toggleCityField() {
+    if (zipField.value) {
+      cityField.disabled = true;
+    } else {
+      cityField.disabled = false;
     }
+  }
 
-    function enableInputs() {
-        cityField.disabled = false;
-        zipField.disabled = false;
-        document.getElementById("win-msg").classList.add("hidden");
-        document.getElementById("tutorial").classList.remove("hidden");
-    }
+  function enableInputs() {
+    cityField.disabled = false;
+    zipField.disabled = false;
+    document.getElementById("win-msg").classList.add("hidden");
+    document.getElementById("tutorial").classList.remove("hidden");
+  }
 
-    function disableInputs() {
-        cityField.disabled = true;
-        zipField.disabled = true;
-        document.getElementById("win-msg").classList.remove("hidden");
-        document.getElementById("tutorial").classList.add("hidden");
-        document.getElementById("input-form").classList.remove("relative");
-        document.getElementById("input-form").classList.add("absolute", "bottom-10");
-    }
+  function disableInputs() {
+    cityField.disabled = true;
+    zipField.disabled = true;
+    document.getElementById("win-msg").classList.remove("hidden");
+    document.getElementById("tutorial").classList.add("hidden");
+    document.getElementById("input-form").classList.remove("relative");
+    document
+      .getElementById("input-form")
+      .classList.add("absolute", "bottom-10");
+  }
 
-    function handleEnterKey(e) {
-        if (e.key === "Enter") {
-            e.preventDefault();
-            handleCurtainClick(e);
-        }
+  function handleEnterKey(e) {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      handleCurtainClick(e);
     }
-    function filterCityInput() {
-        const regex = /[^a-zA-Z\u0080-\u024F .'-]/g;
-        cityField.value = cityField.value.replace(regex, "");
-    }
-    function filterZipInput() {
-        zipField.value = zipField.value.replace(/[^\d]/g, "");
-    }
+  }
+  function filterCityInput() {
+    const regex = /[^a-zA-Z\u0080-\u024F .'-]/g;
+    cityField.value = cityField.value.replace(regex, "");
+  }
+  function filterZipInput() {
+    zipField.value = zipField.value.replace(/[^\d]/g, "");
+  }
 
-    async function openCurtains() {
-        if (!windowElement.classList.contains("-translate-y-full")) {
-            leftCurtain.classList.add("w-[80%]");
-            rightCurtain.classList.add("w-[80%]");
-            setTimeout(() => {
-                leftCurtain.classList.add("-translate-x-full");
-                rightCurtain.classList.add("translate-x-full");
-            }, 1);
-            setTimeout(() => {
-                windowElement.classList.add("-translate-y-full");
-                glassElement.classList.add("-translate-y-full");
-            }, 500);
-            await window.clickedEvent();
-        }
+  async function openCurtains() {
+    if (!windowElement.classList.contains("-translate-y-full")) {
+      leftCurtain.classList.add("w-[80%]");
+      rightCurtain.classList.add("w-[80%]");
+      setTimeout(() => {
+        leftCurtain.classList.add("-translate-x-full");
+        rightCurtain.classList.add("translate-x-full");
+      }, 1);
+      setTimeout(() => {
+        windowElement.classList.add("-translate-y-full");
+        glassElement.classList.add("-translate-y-full");
+      }, 500);
+      await window.clickedEvent();
     }
+  }
 
-    function closeCurtains() {
-        if (windowElement.classList.contains("-translate-y-full")) {
-            windowElement.classList.remove("-translate-y-full");
-            glassElement.classList.remove("-translate-y-full");
-            setTimeout(() => {
-                leftCurtain.classList.remove("-translate-x-full");
-                rightCurtain.classList.remove("translate-x-full");
-            }, 500);
-            setTimeout(() => {
-                leftCurtain.classList.remove("w-[80%]");
-                rightCurtain.classList.remove("w-[80%]");
-            }, 501);
-            if (typeof stopSnippet === "function") {
-                stopSnippet();
-            }
-        }
+  function closeCurtains() {
+    if (windowElement.classList.contains("-translate-y-full")) {
+      windowElement.classList.remove("-translate-y-full");
+      glassElement.classList.remove("-translate-y-full");
+      setTimeout(() => {
+        leftCurtain.classList.remove("-translate-x-full");
+        rightCurtain.classList.remove("translate-x-full");
+      }, 500);
+      setTimeout(() => {
+        leftCurtain.classList.remove("w-[80%]");
+        rightCurtain.classList.remove("w-[80%]");
+      }, 501);
+      if (typeof stopSnippet === "function") {
+        stopSnippet();
+      }
     }
+  }
 
-    function enableCurtainInteraction() {
-        if (cityField) {
-            leftCurtain.style.pointerEvents = "all";
-            rightCurtain.style.pointerEvents = "all";
-        } else {
-            leftCurtain.style.pointerEvents = "none";
-            rightCurtain.style.pointerEvents = "none";
-        }
+  function enableCurtainInteraction() {
+    if (cityField) {
+      leftCurtain.style.pointerEvents = "all";
+      rightCurtain.style.pointerEvents = "all";
+    } else {
+      leftCurtain.style.pointerEvents = "none";
+      rightCurtain.style.pointerEvents = "none";
     }
+  }
 
-    async function handleCurtainClick(e) {
-        e.stopPropagation();
-        if (!cityField) {
-            alert("Please enter a city.");
-            return;
-        }
-        const success = await fetchAndUpdateWeather(
-            cityField.value,
-            zipField.value
-        );
-        if (success) {
-            if (!windowElement.classList.contains("-translate-y-full")) {
-                openCurtains();
-                disableInputs();
-            }
-        } else {
-            closeCurtains();
-        }
+  async function handleCurtainClick(e) {
+    e.stopPropagation();
+    if (!cityField) {
+      alert("Please enter a city.");
+      return;
     }
-
-    function resetForm() {
-        cityField.value = "";
-        zipField.value = "";
-        weatherElement.classList.remove("relative", "bottom-24");
-        temperatureElement.classList.remove("relative", "bottom-24");
-        document.getElementById("input-form").classList.remove("absolute", "bottom-10");
-        document.getElementById("input-form").classList.add("relative");
-        temperatureElement.innerHTML ="What's the weather looking like out there?";
-        weatherElement.innerHTML = "";
-        toggleCityField();
-        enableCurtainInteraction();
+    const success = await fetchAndUpdateWeather(
+      cityField.value,
+      zipField.value
+    );
+    if (success) {
+      if (!windowElement.classList.contains("-translate-y-full")) {
+        openCurtains();
+        disableInputs();
+      }
+    } else {
+      closeCurtains();
     }
+  }
 
+  function resetForm() {
+    cityField.value = "";
+    zipField.value = "";
+    weatherElement.classList.remove("relative", "bottom-24");
+    temperatureElement.classList.remove("relative", "bottom-24");
+    document
+      .getElementById("input-form")
+      .classList.remove("absolute", "bottom-10");
+    document.getElementById("input-form").classList.add("relative");
+    temperatureElement.innerHTML = "What's the weather looking like out there?";
+    weatherElement.innerHTML = "";
+    toggleCityField();
     enableCurtainInteraction();
+  }
+
+  enableCurtainInteraction();
 });
